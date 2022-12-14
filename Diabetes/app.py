df6bba47a70458d91923e97c1fc1600e64b79f77
ValenMaz <valentinamazxd@gmail.com>
2022-12-14 09:43:29 -0500

    deployment

---
+++ Diabetes/app.py
@@ -0,0 +1,259 @@
+import joblib
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_security import current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+import pandas as pd
+import pickle
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from flask_bcrypt import Bcrypt
+from flask_bcrypt import generate_password_hash
+from flask_bcrypt import check_password_hash
+from flask_login import current_user
+from flask import request
+from datetime import datetime
+
+from flask_migrate import Migrate
+filename = 'diabetes-prediction-rfc-model.pkl'
+classifier = pickle.load(open(filename, 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+model1 = pickle.load(open('model1.pkl', 'rb'))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/database'
+#postgresql://postgres:postgres@localhost/database-sqlite:///database.db
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+bcrypt = Bcrypt(app)
+
+
+############################################################################################################
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+    user_predictions = db.relationship('UserPredictions', backref='user')
+
+class UserPredictions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    date = db.Column(db.DateTime)
+    result = db.Column(db.String(25))
+
+def __init__(self, user_id, date, result):
+   self.user_id = user_id
+   self.date = date
+   self.result = result
+   
+
+
+############################################################################################################
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+############################################################################################################
+#Forms.py
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
+
+############################################################################################################
+#views.py
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/help')
+def help():
+    return render_template("help.html")
+
+
+@app.route('/terms')
+def terms():
+    return render_template("tc.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+
+        return render_template("login.html", form=form)
+    return render_template("login.html", form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect("/login")
+    return render_template('signup.html', form=form)
+
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/disindex")
+
+def disindex():
+    return render_template("disindex.html")
+
+############################################################################################################
+
+@app.route("/diabetes")
+@login_required
+def diabetes():
+    return render_template("diabetes.html")
+
+
+
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+##################################################################################
+
+df1 = pd.read_csv('diabetes.csv')
+
+# Renaming DiabetesPedigreeFunction as DPF
+df1 = df1.rename(columns={'DiabetesPedigreeFunction': 'DPF'})
+
+# Replacing the 0 values from ['Glucose','BloodPressure','SkinThickness','Insulin','BMI'] by NaN
+df_copy = df1.copy(deep=True)
+df_copy[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']] = df_copy[['Glucose', 'BloodPressure',
+                                                                                    'SkinThickness', 'Insulin',
+                                                                                    'BMI']].replace(0, np.NaN)
+
+# Replacing NaN value by mean, median depending upon distribution
+df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
+df_copy['BloodPressure'].fillna(df_copy['BloodPressure'].mean(), inplace=True)
+df_copy['SkinThickness'].fillna(df_copy['SkinThickness'].median(), inplace=True)
+df_copy['Insulin'].fillna(df_copy['Insulin'].median(), inplace=True)
+df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
+
+# Model Building
+
+X = df1.drop(columns='Outcome')
+y = df1['Outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# Creating Random Forest Model
+#Fit the model on training set
+
+classifier = RandomForestClassifier(n_estimators=20)
+classifier.fit(X_train, y_train)
+
+# Creating a pickle file for the classifier
+filename = 'diabetes-prediction-rfc-model.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
+accuracy = classifier.score(X_test, y_test)
+
+#print(accuracy)
+
+#####################################################################
+
+
+
+@app.route('/predictt', methods=['GET','POST'])
+def predictt():
+    if request.method == 'POST':
+        preg = request.form['pregnancies']
+        glucose = request.form['glucose']
+        bp = request.form['bloodpressure']
+        st = request.form['skinthickness']
+        insulin = request.form['insulin']
+        bmi = request.form['bmi']
+        dpf = request.form['dpf']
+        age = request.form['age']
+
+        data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
+        my_prediction = classifier.predict(data)
+
+    if my_prediction == 1:
+        res_val = "a high risk of Diabetes"
+    else:
+        res_val = "a low risk of Diabetes"
+    
+    
+    
+    date = datetime.now().strftime('%Y-%m-%d')
+    result = res_val
+
+    
+
+    record = UserPredictions(user_id=current_user.id, date=date, result=result)
+
+
+    # Flask-SQLAlchemy magic adds record to database
+    db.session.add(record)
+    db.session.commit()
+
+
+       
+    return render_template('diab_result.html',  prediction_text='You have {}'.format(res_val))
+
+
+############################################################################################################
+@app.route("/user", methods=['GET','POST'])
+@login_required
+def user():
+    predictions = UserPredictions.query.filter_by(user_id=current_user.id).all() 
+
+    return render_template("user.html", predictions=predictions)
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
